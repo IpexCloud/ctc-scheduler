217818716c020c0b843e46008fdcca3ccc6ecee9
exxon23 <sebastian.mach123@gmail.com>
2022-12-02 16:21:26 +0100

    Added new redis client

--- config/index.ts
+++ config/index.ts
@@ -2,9 +2,11 @@ import { loadEnv } from '@/utils/helpers'
 
 export const PORT = process.env.PORT || '8080'
 
-export const REDIS_PORT = Number(process.env.REDIS_PORT) || 6379
 export const REDIS_HOST = loadEnv('REDIS_HOST')
-export const REDIS_DB_INDEX = process.env.REDIS_DB_INDEX || 0
+export const REDIS_PORT = Number(loadEnv('REDIS_PORT'))
+export const REDIS_DB_INDEX = Number(process.env.REDIS_DB_INDEX || 0)
+export const REDIS_USERNAME = loadEnv('REDIS_USERNAME')
+export const REDIS_PASSWORD = loadEnv('REDIS_PASSWORD')
 
 export const ENVIRONMENT_NAME = process.env.ENVIRONMENT_NAME || 'ctc-scheduler'
 
